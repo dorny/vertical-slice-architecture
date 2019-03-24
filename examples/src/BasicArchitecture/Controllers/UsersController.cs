f8a06bb5c8e40390b6f4dbfeb7a7d6156aa944be
Michal Dorner <dorner.michal@gmail.com>
2019-03-24 23:13:44 +0100

    Basic architecture

---
+++ examples/src/BasicArchitecture/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using BasicArchitecture.Data;
+using BasicArchitecture.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicArchitecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserEntity>> Get()
+        {
+            return _dataContext.Users.ToList();
+        }
+    }
+}
---
+++ examples/src/BasicArchitecture/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using BasicArchitecture.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicArchitecture.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>();
+        }
+    }
+}
